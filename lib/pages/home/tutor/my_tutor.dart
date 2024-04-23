963a49996e71634f15f829cbd7a29982dcaddfc6
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-04-23 22:07:33 +0800

    UI changes on Subject Matter Page

--- lib/pages/home/tutor/my_tutor.dart
+++ lib/pages/home/tutor/my_tutor.dart
@@ -205,14 +205,32 @@ class RegisterAsTutor extends ConsumerWidget {
                                 (e) => Consumer(
                                   builder: (context, ref, child) {
                                     final course = ref.watch(getSubjectInfo(e));
-
                                     return course.when(
                                       data: (data) {
-                                        return Chip(
-                                          label: Text(
-                                            data.subject_code,
-                                            style:
-                                                const TextStyle(fontSize: 10),
+                                        return IntrinsicWidth(
+                                          child: Container(
+                                            margin: const EdgeInsets.symmetric(
+                                              vertical: 4,
+                                            ),
+                                            padding: const EdgeInsets.symmetric(
+                                              vertical: 4,
+                                              horizontal: 8,
+                                            ),
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .tertiaryContainer,
+                                                width: 1,
+                                              ),
+                                              borderRadius:
+                                                  BorderRadius.circular(30),
+                                            ),
+                                            child: Text(
+                                              data.subject_code,
+                                              style:
+                                                  const TextStyle(fontSize: 14),
+                                            ),
                                           ),
                                         );
                                       },

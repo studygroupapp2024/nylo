a664ebfc9df0fc7aa69a74e1e076d077f8a06a2f
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-04-28 18:08:24 +0800

    added staging

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,17 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nylo/app.dart';
+import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nylo/firebase/staging/firebase_options-staging.dart';
+import 'package:nylo/structure/auth/auth_gate.dart';
+import 'package:nylo/structure/messaging/message_api.dart';
+import 'package:nylo/themes/light_mode.dart';
 
-void main() {
-  runApp(const MainApp());
+@pragma('vm:entry-point')
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'nylo-staging',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await Firebase.initializeApp();
+
+  await FirebaseMessage().initNotifications();
+
+  // Set the system navigation bar color here
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: App(flavor: "Staging"),
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      home: const AuthGate(),
+      theme: lightMode,
     );
   }
 }
